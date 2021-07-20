55a92fa746cc3d18c10a567c448b29f0a538ade1
Neil Betham <nbetham@me.com>
2021-07-20 02:04:15 -0400

    Remove need for JQ

--- utils/gh-release-utils.sh
+++ utils/gh-release-utils.sh
@@ -1,42 +1,111 @@
 #!/usr/bin/env bash
 
 # Utils for fetching release assets from GitHub
-# Use the vendored JQ for this script
-JQ="jq-vendor"
 
-function curl_api() {
-  API_URL="$1"
-  FULL_URL="https://api.github.com/${API_URL}"
-  curl -s -H "Accept: application/vnd.github.v3+json" "${FULL_URL}"
+function log_error() {
+  MESSAGE="$1"
+  echo "[ERROR] - ${MESSAGE}"
 }
 
-function jq_filt() {
-  JSON_BLOB="$1"
-  JQ_FILTER="$2"
-  echo "${JSON_BLOB}" | $JQ -c "${JQ_FILTER}"
+function cmd_exists() {
+  command -v "$1" &> /dev/null
+}
+
+function curl_wrap() {
+  URI="$1"
+  HEADERS="$2"
+  OUTPUT_FILE="$3"
+
+  if [ -z "${HEADERS}" ]; then
+    if [ -z "${OUTPUT_FILE}" ]; then
+      curl -sL "${URI}"
+    else
+      curl -sL -o "${OUTPUT_FILE}" "${URI}"
+    fi
+  else
+    if [ -z "${OUTPUT_FILE}" ]; then
+      curl -sL -H "${HEADERS}" "${URI}"
+    else
+      curl -sL -H "${HEADERS}" -o "${OUTPUT_FILE}" "${URI}"
+    fi
+  fi
+}
+
+function wget_wrap() {
+  URI="$1"
+  HEADERS="$2"
+  OUTPUT_FILE="$3"
+
+  if [ -z "${HEADERS}" ]; then
+    if [ -z "${OUTPUT_FILE}" ]; then
+      wget -q "${URI}"
+    else
+      wget -q -O "${OUTPUT_FILE}" "${URI}"
+    fi
+  else
+    if [ -z "${OUTPUT_FILE}" ]; then
+      wget -q --header "${HEADERS}" "${URI}"
+    else
+      wget -q --header "${HEADERS}" -O "${OUTPUT_FILE}" "${URI}"
+    fi
+  fi
+}
+
+function http_download() {
+  URI="$1"
+  HEADERS="$2"
+  OUTPUT_FILE="$3"
+
+  if cmd_exists curl; then
+    curl_wrap "${URI}" "${HEADERS}" "${OUTPUT_FILE}"
+    return
+  elif cmd_exists wget; then
+    wget_wrap "${URI}" "${HEADERS}" "${OUTPUT_FILE}"
+    return
+  fi
+  log_error "No valid http download tool"
+  return 1
+}
+
+function call_gh_api() {
+  API_URL="$1"
+  FULL_URL="https://api.github.com/${API_URL}"
+  http_download "${FULL_URL}" "Accept: application/vnd.github.v3+json"
+}
+
+function filter_json_for_assets() {
+  RELEASES_JSON="$1"
+  echo "${RELEASES_JSON}" \
+    | tr -s '\n' ' ' \
+    | sed 's/.*"assets"//' \
+    | sed 's/browser/\nbrowser/g' \
+    | grep browser \
+    | sed 's/browser_download_url": "//' \
+    | sed 's/".*//' \
+    | tr -s '\n' ' '
 }
 
 function get_latest_release_assets() {
   USER_REPO_PATH="$1"
-  curl_api "repos/${USER_REPO_PATH}/releases?per_page=1&page=1"
+  call_gh_api "repos/${USER_REPO_PATH}/releases?per_page=1&page=1"
 }
 
 function get_latest_release_file() {
   USER_REPO_PATH="$1"
   FILE_NAME_MATCHER="$2"
   LATEST_RELEASE_ASSETS=$(get_latest_release_assets "${USER_REPO_PATH}")
-  while IFS= read -r OBJ; do
-    FILE_NAME="$(jq_filt "${OBJ}" '.name' | tr -d \")"
-    if [[ "${FILE_NAME}" =~ .*"${FILE_NAME_MATCHER}".* ]]; then
-      echo "$(jq_filt "${OBJ}" '.browser_download_url' | tr -d \")"
+  ASSERT_URLS=($(filter_json_for_assets ${LATEST_RELEASE_ASSETS}))
+  for ASSET_URL in "${ASSERT_URLS[@]}"; do
+    if [[ ${ASSET_URL} =~ ${FILE_NAME_MATCHER} ]]; then
+      echo "${ASSET_URL}"
       break
     fi
-  done < <(jq_filt "${LATEST_RELEASE_ASSETS}" '.[0].assets[]')
+  done
 }
 
 function download_release() {
   USER_REPO_PATH="$1"
   FILE_NAME_MATCHER="$2"
   OUTPUT_FILE=$3
-  curl -s -o "${OUTPUT_FILE}" -L "$(get_latest_release_file "${USER_REPO_PATH}" "${FILE_NAME_MATCHER}")"
+  http_download "$(get_latest_release_file "${USER_REPO_PATH}" "${FILE_NAME_MATCHER}")" "" "${OUTPUT_FILE}"
 }
